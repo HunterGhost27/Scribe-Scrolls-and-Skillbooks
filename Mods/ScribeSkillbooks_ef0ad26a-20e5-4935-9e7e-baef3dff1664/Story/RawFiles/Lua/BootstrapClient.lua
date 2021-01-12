3e2c4604d930269f6971e9cb50fc46296c5d79a3
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-12 18:07:50 +0530

    Moved Legacy Compatibility Mode

--- Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapClient.lua
@@ -4,18 +4,4 @@
 
 Ext.Require("Auxiliary.lua")
 if CENTRAL[IDENTIFIER]["ModSettings"]["RecipeGeneration"] then Ext.Require("Client/RecipeGenerator.lua") end
-
---  ========================
---  ITEM COMBO PATH OVERRIDE
---  ========================
-
-Ext.RegisterListener("ModuleLoadStarted", function ()
-    if CENTRAL[IDENTIFIER]["ModSettings"]["LegacyCompatibilityMode"] then
-        Debug:HFWarn("Legacy Compatibility Mode Active. Keeping old ItemCombos.txt")
-    else
-        Ext.AddPathOverride(
-            "Public/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Stats/Generated/ItemCombos.txt",  --  Old ItemCombos
-            "Public/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Stats/Override/ItemCombos.txt"    --  New ItemCombos
-        )
-    end
-end)
+if CENTRAL[IDENTIFIER]["ModSettings"]["LegacyCompatibilityMode"] then Ext.Require("Client/LegacyCompatibilityMode.lua") end
---
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/Client/LegacyCompatibilityMode.lua
@@ -0,0 +1,11 @@
+--  ========================
+--  ITEM COMBO PATH OVERRIDE
+--  ========================
+
+Ext.RegisterListener("ModuleLoadStarted", function ()
+    Debug:HFWarn("Legacy Compatibility Mode Active. Keeping old ItemCombos.txt")
+    Ext.AddPathOverride(
+        "Public/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Stats/Generated/ItemCombos.txt",  --  Old ItemCombos
+        "Public/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Stats/Override/ItemCombos.txt"    --  New ItemCombos
+    )
+end)

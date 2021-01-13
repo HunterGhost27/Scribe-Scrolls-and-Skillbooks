16af242cea8691103372547ccbc816e20d5deb5f
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-13 13:26:15 +0530

    Removed ItemCombinator

--- Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapServer.lua
@@ -3,5 +3,4 @@
 --  =======
 
 Ext.Require("Auxiliary.lua")
-if not MODINFO.ModSettings["RecipeGeneration"] then Ext.Require("Server/ItemCombinator.lua") end
 if Ext.IsDeveloperMode() then Ext.Require('Server/Debug.lua') end
--- Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/Server/Debug.lua
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/Server/Debug.lua
@@ -6,7 +6,6 @@ local function DebugMode()
     local host = Osi.CharacterGetHostCharacter()
     Osi.ItemTemplateAddTo(Template.LOOT_Ink_Pot_A_Quill_A, host, 1, 1)
     Osi.ItemTemplateAddTo(Template.BOOK_Paper_Sheet_A, host, 50, 1)
-    Osi.CharacterAddAbilityPoint(host, 10)
     Osi.CharacterAddSkill(host, 'Target_Apportation', 1)
 end
 

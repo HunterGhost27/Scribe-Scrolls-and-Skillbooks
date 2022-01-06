c639a308f3175fe0563bb9c0ec35165ff6b67464
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2022-01-07 03:00:57 +0530

    Create Quill Recipe

--- Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/BootstrapClient.lua
@@ -5,4 +5,6 @@
 Ext.Require("Auxiliary.lua")
 Ext.Require("Client/TreasureTables.lua")
 Ext.Require("Client/LegacyCompatibilityMode.lua")
+Ext.Require("Client/QuillRecipe.lua")
 if MODINFO.ModSettings["RecipeGeneration"] then Ext.Require("Client/RecipeGenerator.lua") end
+
---
+++ Mods/ScribeSkillbooks_ef0ad26a-20e5-4935-9e7e-baef3dff1664/Story/RawFiles/Lua/Client/QuillRecipe.lua
@@ -0,0 +1,39 @@
+Ext.RegisterListener("StatsLoaded", function ()
+    Ext.UpdateItemCombo({
+        ["AutoLevel"] = false,
+        ["CraftingStation"] = "None",
+        ["Ingredients"] = {
+            [1] = {
+                ["IngredientType"] = "Category",
+                ["ItemRarity"] = "Sentinel",
+                ["Object"] = "CuttingTool",
+                ["Transform"] = "Transform"
+            },
+            [2] = {
+                ["IngredientType"] = "Object",
+                ["ItemRarity"] = "Sentinel",
+                ["Object"] = "LOOT_Feather_A",
+                ["Transform"] = "None"
+            }
+        },
+        ["Name"] = "CuttingTool_LOOT_Feather_A",
+        ["RecipeCategory"] = "Grimoire",
+        ["Results"] = {
+            [1] = {
+                ["Name"] = "CuttingTool_LOOT_Feather_A_1",
+                ["PreviewIcon"] = "",
+                ["PreviewStatsId"] = "LOOT_Feather_A_Quill_A",
+                ["PreviewTooltip"] = "",
+                ["ReqLevel"] = 0,
+                ["Requirement"] = "Sentinel",
+                ["Results"] = {
+                    [1] = {
+                        ["Boost"] = "",
+                        ["Result"] = "LOOT_Feather_A_Quill_A",
+                        ["ResultAmount"] = 1
+                    }
+                }
+            }
+        }
+    })
+end)
